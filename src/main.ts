e17f910aceb0a9fbde702db8b472e23140740a9b
kenforth <yashaar@mail.ru>
2023-12-10 16:06:05 +0700

    Add navigation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./plugins/router";
 import "./assets/scss/global.scss"
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
