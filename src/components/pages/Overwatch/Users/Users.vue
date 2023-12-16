97e93a33e6855b4100e5221917b35aa2ed3bb90c
kenforth <yashaar@mail.ru>
2023-12-17 01:31:49 +0700

    Add type to parameter

--- src/components/pages/Overwatch/Users/Users.vue
+++ src/components/pages/Overwatch/Users/Users.vue
@@ -77,7 +77,7 @@ onBeforeMount(() => {
   });
 })
 
-const addUser  = (user: number) => {
+const addUser  = (user: number | never) => {
   if (usersSeries.value[0].data.length >= 20) {
     return usersSeries.value[0].data.shift()
   }
