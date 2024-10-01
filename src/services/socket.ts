0f00af604c84fbc98d2a5b8e6bf5835b8a31441f
Konstantin Rusanov <rusanov@uteka.ru>
2024-10-01 14:09:40 +0700

    Remove console log

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,22 +2,19 @@ import { reactive } from "vue";
 import { io } from "socket.io-client";
 
 export const state = reactive({
-    connected: false,
-    fooEvents: [],
-    barEvents: []
+  connected: false,
+  fooEvents: [],
+  barEvents: [],
 });
 
-const URL = import.meta.env.VITE_URL
+const URL = import.meta.env.VITE_URL;
 
-console.log('URL', URL)
-
-export const socket = io(URL, { transports : ['websocket'] });
+export const socket = io(URL, { transports: ["websocket"] });
 
 socket.on("connect", () => {
-    state.connected = true;
+  state.connected = true;
 });
 
 socket.on("disconnect", () => {
-    state.connected = false;
+  state.connected = false;
 });
-
